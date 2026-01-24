71ac93ce62243c1c8ed8779da577f6ef28f73822
Alipay <zhpwap@qq.com>
2026-01-24 17:51:05 +0800

    修改spi_lcd例程，适应新版sdk提供的 SDK_VERSION_STRING，其他例程暂未修改

--- SoftwareExamples/spi_lcd/src/spi_lcd.c
+++ SoftwareExamples/spi_lcd/src/spi_lcd.c
@@ -17,7 +17,9 @@ int main(void) {
   board_init_clock();
   SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_NO_BLOCK_TRIM);
   board_print_clock_freq();
-  LOG("SDK_VERSION:%d.%d.%d\r\n", SDK_VERSION_MAJOR, SDK_VERSION_MINOR, SDK_PATCHLEVEL);
+#if defined (SDK_VERSION_STRING)
+  LOG("SDK_VERSION:%s\r\n", SDK_VERSION_STRING);
+#endif
   board_init_led_pins();
   board_init_btn();
   board_timer_create(LED1_FLASH_PERIOD_IN_MS, board_led0_toggle);
